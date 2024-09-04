8d6e7cd6f370f85b087aa04e5e07b24a5c552253
somoneFromHell <somone_from_hell@tutanota.com>
2024-09-04 13:41:51 +0530

    tsc

--- dist/index.js
+++ dist/index.js
@@ -25,6 +25,7 @@ cron.schedule(process.env.CRON_SHEDULE, () => {
     console.log("Running scheduled task at midnight...");
     updateWetherData();
 });
+app.use(express_1.default.static("public"));
 app.set("view engine", "ejs");
 app.set("views", path_1.default.join(__dirname, "../public"));
 app.use((0, cors_1.default)());
